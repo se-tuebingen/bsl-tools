67b7c4c7f473c8fa6a42df9a366088ae7fe75159
Linus S <66415456+linususer@users.noreply.github.com>
2022-05-13 23:04:23 +0200

    testing

--- racket/sexpr-json.rkt
+++ racket/sexpr-json.rkt
@@ -1,18 +1,71 @@
 #lang racket
 (require json)
 (require racket/match)
-
+; module sexpr->json
+; gets sexpr and prints a json file
+; JSON STRUCTURE
+;
+;
+;
+;
+;
+;Js Expr Test
 (define js-expr-test "cheesecake")
 (define sexpr '(+ (* 4 5) 3))
 (define sexpr2 '(1,2,3))
+(define nested-sexpr
+  '(+
+    (/
+      (* 2
+        (+ 3 5)
+        (-
+          (* 8 4)
+            2)
+          2)
+        10)
+      2)
+    )
 sexpr
 
-; sexpr -> List-Of-sexpr
-; (define sexpr->lst (sexpr)
-; (match
-; [( '(exp x)) "okay"]
-; [else "No"]
+
+; sexpr->List
+; '(+
+;     (* 4 5)
+;   3)
+; (define (sexpr->list sexpr)
+; (match sexpr
+;   [(cons a rest) (cons a (cons sexpr->list(rest)'()))]
+;   [else empty]
 ;   )
 ; )
 
-(sexp->list sexpr2)
+; output file test
+(define out (open-output-file "some.json"))
+
+; Jsexpr->Json test
+(define json_output (write-json js-expr-test out))
+json_output
+(close-output-port out)
+
+
+
+; ####YAML-TEST####
+;yaml-expr-TEST
+; (require yaml)
+;
+; (define lonely-string "# hallo")
+; (string->yaml lonely-string)
+;
+; (define strings-for-yaml (string-append
+;     "# Ranking of 1998 home runs\n"
+;     "---\n"
+;     "- Mark McGwire\n"
+;     "- Sammy Sosa\n"
+;     "- Ken Griffey\n"
+;     "\n"
+;     "# Team ranking\n"
+;     "---\n"
+;     "- Chicago Cubs\n"
+;     "- St. Louis Cardinals\n"))
+;
+; (string->yaml* strings-for-yaml)
