80b2897b24c87425575ff39bb5e0d9850531fcad
Florian Kellner <mr.florian.kellner@web.de>
2022-05-17 20:01:44 +0200

    wip: fancy animation when expanding tree (missing: fade-over, correct x/y coordinates)

--- dist/layout.js
+++ dist/layout.js
@@ -8,9 +8,11 @@
 function treeProgram(program, target) {
     target.innerHTML = program.map(treeDefOrExpr).join('\n');
     // align connectors horizontally
-    adjustConnectors('hole-1', 'child-1');
-    adjustConnectors('hole-2', 'child-2');
-    adjustConnectors('hole-3', 'child-3');
+    adjustAllConnectors();
+    // add data where holes should move upon expanding
+    addHoleTranslations();
+    // add collapsers
+    addCollapsers(target);
 }
 // #### render helpers (textual html generators) ####
 function treeDefOrExpr(root) {
@@ -21,7 +23,8 @@ function treeDefinition(d) {
         return `
       <span>
         <div class="name">Function Definition</div>
-        <div>( define ( <div class="hole hole-1">name</div> <div class="hole hole-2">name+</div> ) <div class="hole hole-3">e</div> )</div>
+        <div class="empty">( define ( <div class="hole hole-1">name</div> <div class="hole hole-2">name+</div> ) <div class="hole hole-3">e</div> )</div>
+        <div class="full">( define ( <div class="hole">${pprint([d.fname])}</div> <div class="hole">${d.args.map(printName).join('</div> <div class="hole">')}</div> ) <div class="hole">${pprint([d.body])}</div> )</div>
       </span>
       <ul>
         <li class="child-1">${treeName(d.fname)}</li>
@@ -33,7 +36,8 @@ function treeDefinition(d) {
         return `
       <span>
         <div class="name">Constant Definition</div>
-        <div>( define <div class="hole hole-1">name</div> <div class="hole hole-2">e</div> )</div>
+        <div class="empty">( define <div class="hole hole-1">name</div> <div class="hole hole-2">e</div> )</div>
+        <div class="full">( define <div class="hole">${pprint([d.cname])}</div> <div class="hole">${pprint([d.value])}</div> )</div>
       </span>
       <ul>
         <li class="child-1">${treeName(d.cname)}</li>
@@ -44,7 +48,10 @@ function treeDefinition(d) {
         return `
       <span>
         <div class="name">Struct Definition</div>
-        <div>(define-struct <div class="hole hole-1">name</div> ( <div class="hole hole-2">name*</div> ) )</div>
+
+        <div class="empty">(define-struct <div class="hole hole-1">name</div> ( <div class="hole hole-2">name*</div> ) )</div>
+
+        <div class="full">(define-struct <div class="hole">${pprint([d.binding])}</div> ( <div class="hole">${d.properties.map(printName).join('</div> <div class="hole">')}</div> ) )</div>
       </span>
       <ul>
         <li class="child-1">${treeName(d.binding)}</li>
@@ -60,7 +67,10 @@ function treeE(e) {
         return `
       <span>
         <div class="name">Function Call</div>
-        <div>( <div class="hole hole-1">name</div> <div class="hole hole-2">e*</div> )</div>
+
+        <div class="empty">( <div class="hole hole-1">name</div> <div class="hole hole-2">e*</div> )</div>
+
+        <div class="full">( <div class="hole">${pprint([e.fname])}</div> <div class="hole">${e.args.map(printE).join('</div> <div class="hole">')}</div> )</div>
       </span>
       <ul>
         <li class="child-1">${treeName(e.fname)}</li>
@@ -71,7 +81,8 @@ function treeE(e) {
         return `
       <span>
         <div class="name">Cond-Expression</div>
-        <div>( cond <div class="hole hole-2">[ e e ]+</div> )</div>
+        <div class="empty">( cond <div class="hole hole-2">[ e e ]+</div> )</div>
+        <div class="full" style="text-align: left;">(cond <br><div class="hole">${e.options.map(printOption).join('</div> <div class="hole">')}</div><br>)</div>
       </span>
       <ul>
         <li class="child-2">${e.options.map(treeOption).join('</li><li class="child-2">')}</li>
@@ -96,7 +107,8 @@ function treeOption(o) {
     return `
     <span>
       <div class="name">Cond-Option</div>
-      <div>[ <div class="hole hole-1">e</div> <div class="hole hole-2">e</div> ]</div>
+      <div class="empty">[ <div class="hole hole-1">e</div> <div class="hole hole-2">e</div> ]</div>
+      <div class="full">[ <div class="hole">${pprint([o.condition])}</div> <div class="hole">${pprint([o.result])}</div> ]</div>
     </span>
     <ul>
       <li class="child-1">${treeE(o.condition)}</li>
@@ -114,10 +126,18 @@ function treeName(s) {
 // ###### layout helper ########
 // dynamically compute dimensions of connectors between nodes
 // such that they point to the correct "hole"
+function adjustAllConnectors() {
+    adjustConnectors('hole-1', 'child-1');
+    adjustConnectors('hole-2', 'child-2');
+    adjustConnectors('hole-3', 'child-3');
+}
 function adjustConnectors(holeClass, childClass) {
     Array.from(document.getElementsByClassName(holeClass)).map(h => {
         const el = h;
         const xpos = 0.5 * (el.getBoundingClientRect().x + el.getBoundingClientRect().right);
+        // no layout effect if hole is invisible
+        if (xpos == 0)
+            return;
         // navigate the DOM
         const div = el.parentElement;
         if (!div) {
@@ -158,5 +178,80 @@ function adjustConnectors(holeClass, childClass) {
             cssText += `--connector-width: ${Math.abs(xdiff) + 1}px;`;
             child.style.cssText = cssText;
         });
+    });
+}
+// add information on where holes should move on expansion
+function addHoleTranslations() {
+    Array.from(document.getElementsByClassName('full')).map(d => {
+        const div = d;
+        const holes = Array.from(div.getElementsByClassName('hole'));
+        const span = div.parentElement;
+        if (!span) {
+            console.error("hole without parent");
+            return;
+        }
+        const ul = span.nextElementSibling;
+        if (!ul) {
+            console.error("hole in span without ul sibling");
+            return;
+        }
+        const childNodes = Array.from(ul.children).filter(c => c.tagName == 'LI').flatMap(c => Array.from(c.children)).filter(c => c.tagName == 'SPAN').map(c => Array.from(c.children).filter(c => c.tagName == 'DIV')[1]);
+        if (holes.length != childNodes.length) {
+            console.error(`Holes length: ${holes.length}, child Nodes length: ${childNodes.length}.`);
+            return;
+        }
+        for (let i = 0; i < holes.length; i++) {
+            console.log(`Matching up: ${holes[i].innerHTML} and ${childNodes[i].innerHTML}`);
+            const childX = childNodes[i].getBoundingClientRect().x;
+            const childY = childNodes[i].getBoundingClientRect().y;
+            holes[i].setAttribute('data-translate-x', `${childX}`);
+            holes[i].setAttribute('data-translate-y', `${childY}`);
+        }
+    });
+}
+// collapse/expand nodes
+function addCollapsers(target) {
+    Array.from(target.getElementsByTagName('span')).map(l => {
+        l.onclick = () => {
+            const li = l;
+            if (li.getAttribute('data-collapsed')) {
+                // move holes where children will be
+                const full = Array.from(li.children).filter(c => c.classList.contains('full'))[0];
+                if (!full) {
+                    console.error('expanding node without full div in span', li);
+                    return;
+                }
+                Array.from(full.children).filter(c => c.classList.contains('hole')).map(c => {
+                    console.log('Moving', c);
+                    const el = c;
+                    const currX = c.getBoundingClientRect().x;
+                    const currY = c.getBoundingClientRect().y;
+                    const shouldX = Math.floor(parseFloat(el.getAttribute('data-translate-x')));
+                    const shouldY = Math.floor(parseFloat(el.getAttribute('data-translate-y')));
+                    el.style.cssText = `
+            --translate-x: ${shouldX - currX}px;
+            --translate-y: 3em;
+          `; // TODO: fix y coordinate, handle centering
+                    console.log(el);
+                    window.setTimeout(() => {
+                        el.style.cssText = '';
+                    }, 1100);
+                });
+                // wait until animation has finished
+                window.setTimeout(() => {
+                    li.removeAttribute('data-collapsed');
+                }, 1000);
+            }
+            else {
+                li.setAttribute('data-collapsed', 'true');
+                const ul = li.nextElementSibling;
+                if (!ul)
+                    return;
+                Array.from(ul.getElementsByTagName('span')).map(s => {
+                    s.setAttribute('data-collapsed', 'true');
+                });
+            }
+            // adjustAllConnectors(); unnecessary for display:hidden
+        };
     });
 }
--- src/layout.ts
+++ src/layout.ts
@@ -9,9 +9,11 @@
 function treeProgram(program: BSL_AST.program, target: HTMLElement){
   target.innerHTML = program.map(treeDefOrExpr).join('\n');
   // align connectors horizontally
-  adjustConnectors('hole-1','child-1');
-  adjustConnectors('hole-2','child-2');
-  adjustConnectors('hole-3','child-3');
+  adjustAllConnectors();
+  // add data where holes should move upon expanding
+  addHoleTranslations();
+  // add collapsers
+  addCollapsers(target);
 }
 
 // #### render helpers (textual html generators) ####
@@ -24,7 +26,8 @@ function treeDefinition(d: BSL_AST.definition) {
     return `
       <span>
         <div class="name">Function Definition</div>
-        <div>( define ( <div class="hole hole-1">name</div> <div class="hole hole-2">name+</div> ) <div class="hole hole-3">e</div> )</div>
+        <div class="empty">( define ( <div class="hole hole-1">name</div> <div class="hole hole-2">name+</div> ) <div class="hole hole-3">e</div> )</div>
+        <div class="full">( define ( <div class="hole">${pprint([d.fname])}</div> <div class="hole">${d.args.map(printName).join('</div> <div class="hole">')}</div> ) <div class="hole">${pprint([d.body])}</div> )</div>
       </span>
       <ul>
         <li class="child-1">${treeName(d.fname)}</li>
@@ -35,7 +38,8 @@ function treeDefinition(d: BSL_AST.definition) {
     return `
       <span>
         <div class="name">Constant Definition</div>
-        <div>( define <div class="hole hole-1">name</div> <div class="hole hole-2">e</div> )</div>
+        <div class="empty">( define <div class="hole hole-1">name</div> <div class="hole hole-2">e</div> )</div>
+        <div class="full">( define <div class="hole">${pprint([d.cname])}</div> <div class="hole">${pprint([d.value])}</div> )</div>
       </span>
       <ul>
         <li class="child-1">${treeName(d.cname)}</li>
@@ -45,7 +49,10 @@ function treeDefinition(d: BSL_AST.definition) {
     return `
       <span>
         <div class="name">Struct Definition</div>
-        <div>(define-struct <div class="hole hole-1">name</div> ( <div class="hole hole-2">name*</div> ) )</div>
+
+        <div class="empty">(define-struct <div class="hole hole-1">name</div> ( <div class="hole hole-2">name*</div> ) )</div>
+
+        <div class="full">(define-struct <div class="hole">${pprint([d.binding])}</div> ( <div class="hole">${d.properties.map(printName).join('</div> <div class="hole">')}</div> ) )</div>
       </span>
       <ul>
         <li class="child-1">${treeName(d.binding)}</li>
@@ -61,7 +68,10 @@ function treeE(e: BSL_AST.expr): string {
     return `
       <span>
         <div class="name">Function Call</div>
-        <div>( <div class="hole hole-1">name</div> <div class="hole hole-2">e*</div> )</div>
+
+        <div class="empty">( <div class="hole hole-1">name</div> <div class="hole hole-2">e*</div> )</div>
+
+        <div class="full">( <div class="hole">${pprint([e.fname])}</div> <div class="hole">${e.args.map(printE).join('</div> <div class="hole">')}</div> )</div>
       </span>
       <ul>
         <li class="child-1">${treeName(e.fname)}</li>
@@ -71,7 +81,8 @@ function treeE(e: BSL_AST.expr): string {
     return `
       <span>
         <div class="name">Cond-Expression</div>
-        <div>( cond <div class="hole hole-2">[ e e ]+</div> )</div>
+        <div class="empty">( cond <div class="hole hole-2">[ e e ]+</div> )</div>
+        <div class="full" style="text-align: left;">(cond <br><div class="hole">${e.options.map(printOption).join('</div> <div class="hole">')}</div><br>)</div>
       </span>
       <ul>
         <li class="child-2">${e.options.map(treeOption).join('</li><li class="child-2">')}</li>
@@ -94,7 +105,8 @@ function treeOption(o: BSL_AST.Clause) {
   return `
     <span>
       <div class="name">Cond-Option</div>
-      <div>[ <div class="hole hole-1">e</div> <div class="hole hole-2">e</div> ]</div>
+      <div class="empty">[ <div class="hole hole-1">e</div> <div class="hole hole-2">e</div> ]</div>
+      <div class="full">[ <div class="hole">${pprint([o.condition])}</div> <div class="hole">${pprint([o.result])}</div> ]</div>
     </span>
     <ul>
       <li class="child-1">${treeE(o.condition)}</li>
@@ -114,10 +126,17 @@ function treeName(s: BSL_AST.Name): string {
 // ###### layout helper ########
 // dynamically compute dimensions of connectors between nodes
 // such that they point to the correct "hole"
+function adjustAllConnectors() {
+  adjustConnectors('hole-1','child-1');
+  adjustConnectors('hole-2','child-2');
+  adjustConnectors('hole-3','child-3');
+}
 function adjustConnectors(holeClass:string, childClass: string) {
   Array.from(document.getElementsByClassName(holeClass)).map(h => {
     const el: HTMLElement = h as HTMLElement;
     const xpos = 0.5 * (el.getBoundingClientRect().x + el.getBoundingClientRect().right) ;
+    // no layout effect if hole is invisible
+    if (xpos == 0) return;
 
     // navigate the DOM
     const div = el.parentElement as HTMLElement;
@@ -159,5 +178,83 @@ function adjustConnectors(holeClass:string, childClass: string) {
       child.style.cssText = cssText;
     });
 
+  });
+}
+// add information on where holes should move on expansion
+function addHoleTranslations() {
+  Array.from(document.getElementsByClassName('full')).map(d => {
+    const div: HTMLElement = d as HTMLElement;
+    const holes = Array.from(div.getElementsByClassName('hole'));
+
+    const span: HTMLElement = div.parentElement as HTMLElement;
+    if(!span) { console.error("hole without parent"); return; }
+    const ul: HTMLElement = span.nextElementSibling as HTMLElement;
+    if(!ul) { console.error("hole in span without ul sibling"); return; }
+
+    const childNodes = Array.from(ul.children
+      ).filter(c => c.tagName == 'LI'
+      ).flatMap(c => Array.from(c.children)
+      ).filter(c => c.tagName == 'SPAN'
+    ).map(c => Array.from(c.children).filter(c => c.tagName == 'DIV')[1]);
+
+    if(holes.length != childNodes.length) {
+      console.error(`Holes length: ${holes.length}, child Nodes length: ${childNodes.length}.`);
+      return;
+    }
+    for(let i = 0; i < holes.length; i++) {
+      console.log(`Matching up: ${holes[i].innerHTML} and ${childNodes[i].innerHTML}`);
+      const childX = childNodes[i].getBoundingClientRect().x;
+      const childY = childNodes[i].getBoundingClientRect().y;
+
+      holes[i].setAttribute('data-translate-x', `${childX}`);
+      holes[i].setAttribute('data-translate-y', `${childY}`);
+    }
+
+  });
+}
+
+// collapse/expand nodes
+function addCollapsers(target: HTMLElement) {
+  Array.from(target.getElementsByTagName('span')).map(l => {
+    l.onclick = () => {
+      const li : HTMLElement  = l as HTMLElement;
+      if(li.getAttribute('data-collapsed')) {
+        // move holes where children will be
+        const full = Array.from(li.children).filter(c => c.classList.contains('full'))[0] as HTMLElement;
+        if(!full) { console.error('expanding node without full div in span', li); return; }
+
+        Array.from(full.children).filter(
+          c => c.classList.contains('hole')
+        ).map(c => {
+          console.log('Moving',c);
+          const el = c as HTMLElement;
+          const currX = c.getBoundingClientRect().x;
+          const currY = c.getBoundingClientRect().y;
+          const shouldX = Math.floor(parseFloat(el.getAttribute('data-translate-x') as string));
+          const shouldY = Math.floor(parseFloat(el.getAttribute('data-translate-y') as string));
+          el.style.cssText = `
+            --translate-x: ${shouldX - currX}px;
+            --translate-y: 3em;
+          `; // TODO: fix y coordinate, handle centering
+          console.log(el);
+          window.setTimeout(() => {
+            el.style.cssText = '';
+          }, 1100);
+        });
+
+        // wait until animation has finished
+        window.setTimeout(() => {
+          li.removeAttribute('data-collapsed');
+        }, 1000);
+      } else {
+        li.setAttribute('data-collapsed', 'true');
+        const ul = li.nextElementSibling as HTMLElement;
+        if(!ul) return;
+        Array.from(ul.getElementsByTagName('span')).map(s => {
+          s.setAttribute('data-collapsed', 'true');
+        });
+      }
+      // adjustAllConnectors(); unnecessary for display:hidden
+    }
   });
 }
