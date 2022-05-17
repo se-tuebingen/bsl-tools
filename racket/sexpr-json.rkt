d2e9a4d66b2bb9ea09da7c8cbea76047b02262e2
Linus S <66415456+linususer@users.noreply.github.com>
2022-05-17 18:44:05 +0200

    testing: sexpr-> still not working

--- racket/sexpr-json.rkt
+++ racket/sexpr-json.rkt
@@ -1,11 +1,18 @@
 #lang racket
 (require json)
 (require racket/match)
+(require racket/string)
+(require racket/contract)
+
+
+
+
 ; module sexpr->json
 ; gets sexpr and prints a json file
 ; JSON STRUCTURE
-;
-;
+; {"data": ["+",
+;              ["*", 4, 5],
+;              3]}
 ;
 ;
 ;
@@ -14,38 +21,55 @@
 (define sexpr '(+ (* 4 5) 3))
 (define sexpr2 '(1,2,3))
 (define nested-sexpr
-  '(+
-    (/
-      (* 2
-        (+ 3 5)
-        (-
-          (* 8 4)
-            2)
-          2)
-        10)
-      2)
+  "(+ (/ (* 2(+ 3 5)(-(* 8 4) 2) 2) 10) 2)"
     )
-sexpr
 
+nested-sexpr
+(jsexpr? nested-sexpr)
 
-; sexpr->List
+; sexpr->List of Lists and Symbols
 ; '(+
 ;     (* 4 5)
 ;   3)
-; (define (sexpr->list sexpr)
-; (match sexpr
-;   [(cons a rest) (cons a (cons sexpr->list(rest)'()))]
-;   [else empty]
-;   )
-; )
+
+(define (sexpr->list sexpr)
+  (cond
+    [(empty? sexpr)empty]
+    [(symbol? (first sexpr))(cons(symbol->string (first sexpr))(sexpr->list(rest sexpr)))]
+    [(cons? sexpr) (cons (first sexpr) (sexpr->list (rest sexpr)))]
+    )
+)
+
+(list?(sexpr->list sexpr))
+(jsexpr? (sexpr->list sexpr))
+;list->jsexpr-list
+; ListOfSymbols -> JSExpression List
+; (define (slist->jslist slst)
+;   (cond
+;     [(empty? slst) empty]
+;     [(and (list? (first slst)) (not (symbol? (first slst))))
+;     (cons (string-join (first (first slst)))
+;     (string-join (slist->jslist (first (rest slst)))))]
+;
+;     [(and (cons? slst) (not (symbol? (first slst))))
+;     (cons(string-join (first slst))
+;     (string-join (slist->jslist (rest slst))))]
+;
+;     [(cons? slst)(cons(string-append
+;       (symbol->string(first slst)))
+;       (string-join (slist->jslist (rest slst))))]
+;     )
+;     )
+; (slist->jslist sexpr)
+;(slist->string sexpr)
 
 ; output file test
-(define out (open-output-file "some.json"))
+;(define out (open-output-file "some.json"))
 
 ; Jsexpr->Json test
-(define json_output (write-json js-expr-test out))
-json_output
-(close-output-port out)
+; (define json_output (write-json js-expr-test out))
+; json_output
+; (close-output-port out)
 
 
 
