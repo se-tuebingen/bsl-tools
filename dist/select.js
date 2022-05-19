479750719452d1b8ed61bf7d64d7989a54fedce7
Florian auf Tulpenlaptop <mr.florian.kellner@posteo.de>
2022-05-19 17:15:04 +0200

    POC WIP

---
+++ dist/select.js
@@ -0,0 +1,26 @@
+"use strict";
+function makeTextSelectable(container) {
+    const text = container.innerHTML;
+    // divide up into spans
+    container.innerHTML = text.split('').map(c => `<span>${c}</span>`).join('');
+    // register if mouse is pressed
+    container.onmousedown = () => {
+        container.setAttribute('data-selecting', 'true');
+        Array.from(container.children).map(c => c.removeAttribute('data-selected'));
+    };
+    container.onmouseup = () => {
+        container.removeAttribute('data-selecting');
+    };
+    container.onmouseleave = () => {
+        container.removeAttribute('data-selecting');
+    };
+    // select on mouse over
+    Array.from(container.children).map(c => {
+        const ch = c;
+        ch.onmouseenter = () => {
+            if (ch.parentElement?.getAttribute('data-selecting')) {
+                c.setAttribute('data-selected', 'true');
+            }
+        };
+    });
+}
