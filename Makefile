f32985fd8933268b1255f71268beffb08a4a2e96
linususer <66415456+linususer@users.noreply.github.com>
2022-06-06 21:50:45 +0000

    Deploying to gh-pages from @ se-tuebingen/bsl-tools@0de2d7cdcfe11361731d843a545509455493f992 🚀

--- Makefile
+++
@@ -1,8 +0,0 @@
-build:
-		npm run build
-
-test:
-		cd tests/javascript && cp ../../dist/bsl_tools.js bsl_tools.js
-		
-		cd tests/scribble && ./test.sh
-
--- src/custom.d.ts
+++
@@ -1,5 +0,0 @@
-// needed to be able to import css files as modules containing text
-declare module '*.css' {
-  const content: any;
-  export default content;
-}
