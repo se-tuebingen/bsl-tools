bf8dbe12bed6d0fb57e317b28b436a07bfd21b4b
Florian Kellner <mr.florian.kellner@web.de>
2022-06-14 17:42:21 +0200

    wip: helper structure and transform function

--- src/BSL_Tree.ts
+++ src/BSL_Tree.ts
@@ -482,3 +482,242 @@ function addCollapsers(target: HTMLElement) {
     }
   });
 }
+
+
+
+// ##### simple helper structure representing a printed tree
+interface node {
+  production: string;
+  code: string;
+  holes: {start:number, end:number, content:node}[];
+}
+
+function isLeafNode(n: node):boolean {
+  return n.holes.length <= 0;
+}
+
+// ###### transform AST into helper structure
+function programToNode(p: BSL_AST.program): node {
+  const expressions = p.map(BSL_Print.printDefOrExpr);
+  const indices = [];
+  let program = '';
+  for(let i = 0; i < expressions.length; i++) {
+    const start = program.length;
+    program = `${program}${expressions[i]}`;
+    const end = program.length;
+    indices.push({start:start,end:end});
+    program = `${program}\n`;
+  }
+  return {
+    production: '<program>',
+    code: program,
+    holes: [
+      {
+        start: 0,
+        end: program.length,
+        content: {
+          production: '<def-or-expr>*',
+          code: program,
+          holes: indices.map((h,idx) => { return {
+            start: h.start,
+            end: h.end,
+            content: defOrExprToNode(p[idx])
+          }})
+        }
+      }
+    ]
+  };
+}
+
+function defOrExprToNode(de: BSL_AST.defOrExpr):node {
+  const code = BSL_Print.printDefOrExpr(de) as string;
+  return {
+    production: '<def-or-expr>',
+    code: code,
+    holes: [{
+      start: 0,
+      end: code.length,
+      content: BSL_AST.isDefinition(de) ? defToNode(de) : expToNode(de)
+    }]
+  }
+}
+
+function defToNode(d: BSL_AST.definition):node {
+  let code = '';
+  const holes = [];
+  if (BSL_AST.isFunDef(d)) {
+    code = '( define ( ';
+    let start = code.length;
+    code = `${code}${BSL_Print.printName(d.name)}`;
+    holes.push({start:start, end:code.length, content:nameToNode(d.name)});
+
+    code = `${code} `;
+    start = code.length;
+    code = `${code}${d.args.map(BSL_Print.printName).join(' ')}`;
+    holes.push({start:start, end:code.length, content:namePlusToNode(d.args)});
+
+    code = `${code} ) `;
+    start = code.length;
+    code = `${code} ${BSL_Print.printE(d.body)}`;
+    holes.push({start:start, end:code.length, content:expToNode(d.body)});
+
+    code = `${code} )`;
+  } else if (BSL_AST.isConstDef(d)) {
+    code = '( define ';
+    let start = code.length;
+    code = `${code}${BSL_Print.printName(d.name)}`;
+    holes.push({start:start, end:code.length, content:nameToNode(d.name)});
+
+    code = `${code} `;
+    start = code.length;
+    code = `${code}${BSL_Print.printE(d.value)}`;
+    holes.push({start:start, end:code.length, content:expToNode(d.value)});
+
+    code = `${code} )`;
+  } else {
+    code = '( define-struct ';
+    let start = code.length;
+    code = `${code}${BSL_Print.printName(d.binding)}`;
+    holes.push({start:start, end:code.length, content:nameToNode(d.binding)});
+
+    code = `${code} ( `;
+    start = code.length;
+    code = `${code}${d.properties.map(BSL_Print.printName).join(' ')}`;
+    holes.push({start:start, end:code.length, content:nameStarToNode(d.properties)});
+
+    code = `${code} ) )`;
+  }
+  return {
+    production: `<definition>`,
+    code: code,
+    holes: holes
+  };
+}
+
+function expToNode(e: BSL_AST.expr):node {
+  let code = '';
+  const holes = [];
+  if(BSL_AST.isCall(e)) {
+    code = '( ';
+    let start = code.length;
+    code = `${code}${BSL_Print.printName(e.name)}`;
+    holes.push({start:start, end:code.length, content:nameToNode(e.name)});
+
+    code = `${code} `;
+    start = code.length;
+    code = `${code}${e.args.map(BSL_Print.printE).join(' ')}`;
+    holes.push({start:start, end:code.length, content:eStarToNode(e.args)});
+
+    code = `${code} )`;
+  } else if (BSL_AST.isCond(e)) {
+    code = '( cond ';
+    let start = code.length;
+    code = `${code}${e.options.map(BSL_Print.printOption).join(' ')}`;
+    holes.push({start:start, end:code.length, content:optionsToNode(e.options)});
+    code = `${code} )`;
+  } else if (BSL_AST.isName(e)) {
+    code = BSL_Print.printName(e);
+    holes.push({start:0, end:code.length, content:nameToNode(e)});
+  } else {
+    code = BSL_Print.printE(e);
+    holes.push({start:0, end:code.length, content:literalToNode(e)});
+  }
+  return {
+    production: '<e>',
+    code: code,
+    holes: holes
+  };
+}
+
+function nameToNode(n: BSL_AST.Name):node {
+  return {
+    production: '<name>',
+    code: BSL_Print.printName(n),
+    holes: []
+  };
+}
+
+function namePlusToNode(ns: BSL_AST.Name[]):node {
+  if (ns.length < 1) console.error('<name>+ but used but names are empty');
+  let code = '';
+  const holes = [];
+  for(let i = 0; i < ns.length; i++) {
+    const start = code.length;
+    code = `${code}${BSL_Print.printName(ns[i])}`;
+    holes.push({start:start, end:code.length, content:nameToNode(ns[i])});
+
+    if(i < ns.length - 1) code = `${code} `;
+  }
+  return {
+    production: '<name>+',
+    code: code,
+    holes: holes
+  }
+}
+
+function nameStarToNode(ns: BSL_AST.Name[]):node {
+  let code = '';
+  const holes = [];
+  for(let i = 0; i < ns.length; i++) {
+    const start = code.length;
+    code = `${code}${BSL_Print.printName(ns[i])}`;
+    holes.push({start:start, end:code.length, content:nameToNode(ns[i])});
+
+    if(i < ns.length - 1) code = `${code} `;
+  }
+  return {
+    production: '<name>*',
+    code: code,
+    holes: holes
+  }
+}
+
+function eStarToNode(es: BSL_AST.expr[]):node {
+  let code = '';
+  const holes = [];
+  for(let i = 0; i < es.length; i++) {
+    const start = code.length;
+    code = `${code}${BSL_Print.printE(es[i])}`;
+    holes.push({start:start, end:code.length, content:expToNode(es[i])});
+
+    if(i < es.length - 1) code = `${code} `;
+  }
+  return {
+    production: '<e>*',
+    code: code,
+    holes: holes
+  }
+}
+
+function optionsToNode(os: BSL_AST.Clause[]):node {
+  let code = '';
+  const holes = [];
+  for(let i = 0; i < os.length; i++) {
+    code = `${code}[ `;
+    let start = code.length;
+    code = `${code}${BSL_Print.printE(os[i].condition)}`;
+    holes.push({start:start, end:code.length, content:expToNode(os[i].condition)});
+
+    code = `${code} `;
+    start = code.length;
+    code = `${code}${BSL_Print.printE(os[i].result)}`;
+    holes.push({start:start, end:code.length, content:expToNode(os[i].result)});
+
+    code = `${code} ]`;
+
+    if(i < os.length - 1) code = `${code} `;
+  }
+  return {
+    production: '{[ <e> <e> ]}+',
+    code: code,
+    holes: holes
+  }
+}
+
+function literalToNode(v: BSL_AST.Literal):node {
+  return {
+    production: '<v>',
+    code: BSL_Print.printE(v),
+    holes: []
+  }
+}
