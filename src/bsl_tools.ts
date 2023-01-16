663483a8fc35d017f1d2337b2a53f657363d5497
Florian auf Tulpenlaptop <mr.florian.kellner@posteo.de>
2023-01-16 14:28:29 +0100

    do not break other scripts listening to load event

--- src/bsl_tools.ts
+++ src/bsl_tools.ts
@@ -3,8 +3,10 @@ import {processJsonTrees} from './JSON_Tree';
 import {processSteppers} from './SI_Renderer';
 
 // setup callbacks
-window.onload = () => {
+// - do not set window.onload - other scripts might use it, too
+// - 'load' and not 'DOMContentLoaded' since we want CSS to be ready when we "measure" layout-related stuff
+window.addEventListener('load', () => {
   processBslTrees();
   processJsonTrees();
   processSteppers();
-}
+});
