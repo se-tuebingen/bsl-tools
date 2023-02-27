e394e2e227c80c1bf478206b90611e1a60752d6e
Florian auf Tulpenlaptop <mr.florian.kellner@posteo.de>
2023-02-27 18:04:29 +0100

    add generator element

--- src/Generator.ts
+++ src/Generator.ts
@@ -2,6 +2,9 @@
 import { parse } from "./BSL_Parser";
 import * as BSL_AST from "./BSL_AST";
 import { getParentClassRecursive, navigateDOM } from "./DOM_Helpers";
+import { setUpStepperGui } from "./SI_Renderer";
+import { programToNode, productions } from "./BSL_Tree";
+import { productionTree } from "./Production_Tree";
 
 // # main function: generate a tree or a stepper through <generator> tag
 export function processGenerators(): void {
@@ -22,16 +25,16 @@ export function initGenerator(el: Element) {
         <label>
           <p>Enter valid BSL Code here:</p>
           <textarea id="code" cols="80" rows="5">
-            (define f (* x 2))
-            (f 21)
+(define (f x) (* x 2))
+(f 21)
           </textarea>
         </label><br>
         <button onclick="bsl_tools_generate(event)" data-type="tree">
           Generate Tree
-        </button><br>
+        </button> |
         <button onclick="bsl_tools_generate(event)" data-type="tree-quiz">
           Generate Tree Quiz
-        </button><br>
+        </button> | 
         <button onclick="bsl_tools_generate(event)" data-type="stepper">
           Generate Stepper
         </button>
@@ -41,8 +44,13 @@ export function initGenerator(el: Element) {
     </div>`;
 }
 (window as any).bsl_tools_generate = (e: Event) => {
+  // setup
   const button = e.target as HTMLElement;
   const type = button.getAttribute("data-type");
+  if(!type || !['tree', 'tree-quiz', 'stepper'].includes(type)) {
+    console.error("invalid attribute data-type, must be one of tree, tree-quiz, stepper", type);
+    return;
+  }
   const input = getParentClassRecursive(button, 'input');
   if (!input) {
     console.error("generate_tree called from button not within input div");
@@ -50,6 +58,8 @@ export function initGenerator(el: Element) {
   }
   const textarea = input.getElementsByTagName("textarea")[0];
   const code = textarea.value;
+
+  // parse Program
   let program: BSL_AST.program = [];
   try {
     program = parse(code);
@@ -58,17 +68,35 @@ export function initGenerator(el: Element) {
     return;
   }
 
+  // generate input piece
   const output = navigateDOM([input], '../.output')[0];
   const div = document.createElement('div');
   div.classList.add("generated");
-  output.appendChild(div);
+  div.style.marginTop = "2em";
+  div.style.borderBottom = "1px solid grey";
+  output.prepend(div);
   const innerDiv = document.createElement('div');
   div.appendChild(innerDiv);
   const removePart = document.createElement('div');
+  removePart.style.marginBottom = "1em";
   removePart.innerHTML = `
-    <button onclick="bsl_tools_remove(event)">Remove</button>
+    <button onclick="bsl_tools_remove(event)">Remove ${type} above</button>
   `;
   div.appendChild(removePart);
+
+  // actually generate stuff
+  let quiz = false;
+  switch(type) {
+    case 'stepper':
+      setUpStepperGui(program, innerDiv);
+      return;
+    case 'tree-quiz':
+      quiz = true;
+    case 'tree':
+      const root = programToNode(program);
+      productionTree(root, innerDiv, productions, quiz, 'en');
+      return;
+  }
 }
 
 (window as any).bsl_tools_remove = (e: Event) => {
@@ -80,30 +108,3 @@ export function initGenerator(el: Element) {
   }
   generated.remove();
 }
-
-
-
-
-// ######### RENDERING ########
-
-export function renderGenerator(el: Element): string {
-    return ``
-}
-
-
-// ######### EVENT HANDLERS ########
-// # event handler for <generator> tag
-/*export function onGeneratorClick(e: Event): void {
-    e.preventDefault();
-    const el = e.target as HTMLElement;
-    const id = el.getAttribute("data-id");
-    const type = el.getAttribute("data-type");
-    const generator = document.getElementById(id);
-    if (generator) {
-        if (type === "tree") {
-        generator.innerHTML = renderGenerator(generator);
-        } else if (type === "stepper") {
-        generator.innerHTML = renderGenerator(generator);
-        }
-    }
-}*/
